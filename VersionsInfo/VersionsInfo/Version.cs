ed4807336e7eaac05645b8a3e75e0492891f20b9
Alexandr Drobyshev <alexanderdr@123software.ru>
2018-01-06 18:37:53 +0200

    Added resources, refactored. Works for Android

--- VersionsInfo/VersionsInfo/Version.cs
+++ VersionsInfo/VersionsInfo/Version.cs
@@ -4,15 +4,20 @@ namespace VersionsInfo
 {
     public class Version
     {
-        public string CodeName { get; set; }
         public string VersionNumber { get; set; }
-        public string Description { get; set; }
+        public string CodeName { get; set; }
+        public string Api { get; set; }
+        public string Date { get; set; }
         public ImageSource ImagePath { get; set; }
+        public string Description { get; set; }
 
-        public Version(string versionNumber, string codeName, string description)
+        public Version(string versionNumber, string codeName, string api, string date, ImageSource imagePath, string description)
         {
             VersionNumber = versionNumber;
             CodeName = codeName;
+            Api = api;
+            Date = date;
+            ImagePath = imagePath;
             Description = description;
         }
     }
